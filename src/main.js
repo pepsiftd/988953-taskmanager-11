4002154c1812ea7eb45ff1216f4a47d2f7425092
pepsiftd <pepsiftd@gmail.com>
2020-04-12 21:08:13 +0300

    Реализует добавление тасков по нажатию кнопки

--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import {createLoadMoreButtonTemplate} from "./components/load-more-button.js";
 import {generateFilters} from "./components/mock/filters.js";
 import {generateTasks} from "./components/mock/tasks.js";
 
-const TASK_COUNT = 3;
+const TASK_COUNT = 22;
+const SHOWING_TASKS_COUNT_ON_START = 8;
+const SHOWING_TASKS_COUNT_BY_BUTTON = 8;
 
 const render = (container, template, place) => {
   container.insertAdjacentHTML(place, template);
@@ -29,8 +31,24 @@ const tasks = generateTasks(TASK_COUNT);
 
 render(tasksContainerElement, createTaskEditTemplate(tasks[0]), `afterbegin`);
 
-for (let i = 1; i < tasks.length; i++) {
-  render(tasksContainerElement, createTaskTemplate(tasks[i]), `beforeend`);
-}
+let showingTasksCount = SHOWING_TASKS_COUNT_ON_START;
+
+tasks.slice(1, showingTasksCount)
+  .forEach((task) => render(tasksContainerElement, createTaskTemplate(task), `beforeend`));
+
 
 render(boardElement, createLoadMoreButtonTemplate(), `beforeend`);
+
+const loadMoreButton = boardElement.querySelector(`.load-more`);
+
+loadMoreButton.addEventListener(`click`, () => {
+  const prevTasksCount = showingTasksCount;
+  showingTasksCount = showingTasksCount + SHOWING_TASKS_COUNT_BY_BUTTON;
+
+  tasks.slice(prevTasksCount, showingTasksCount)
+    .forEach((task) => render(tasksContainerElement, createTaskTemplate(task), `beforeend`));
+
+  if (showingTasksCount >= tasks.length) {
+    loadMoreButton.remove();
+  }
+});
