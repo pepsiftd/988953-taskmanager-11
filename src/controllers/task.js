fcb78988d215a678126a0768bc3eb22636e925fc
pepsiftd <pepsiftd@gmail.com>
2020-05-17 22:01:51 +0300

    Добавляет проверку наличия карточки при рендере

--- src/controllers/task.js
+++ src/controllers/task.js
@@ -3,14 +3,17 @@ import TaskEditComponent from "@/components/task-edit.js";
 import {render, replace, RenderPosition} from "@/utils/render.js";
 
 export default class TaskController {
-  constructor(container) {
+  constructor(container, onDataChange) {
     this._container = container;
+    this._onDataChange = onDataChange;
 
     this._taskComponent = null;
     this._taskEditComponent = null;
   }
 
   render(task) {
+    const oldTaskComponent = this._taskComponent;
+    const oldTaskEditComponent = this._taskEditComponent;
     this._taskComponent = new TaskComponent(task);
     this._taskEditComponent = new TaskEditComponent(task);
 
@@ -18,14 +21,34 @@ export default class TaskController {
       this._replaceTaskWithEdit();
     };
 
-    const editFormSubmitHandler = () => {
+    const editFormSubmitHandler = (evt) => {
+      evt.preventDefault();
       this._replaceEditWithTask();
+    };
+
+    const archiveButtonClickHandler = () => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isArchive: !task.isArchive,
+      }));
+    };
+
+    const favoriteButtonClickHandler = () => {
+      this._onDataChange(this, task, Object.assign({}, task, {
+        isFavorite: !task.isFavorite,
+      }));
     };
 
     this._taskComponent.setEditButtonClickHandler(editButtonClickHandler);
     this._taskEditComponent.setSubmitHandler(editFormSubmitHandler);
+    this._taskComponent.setArchiveButtonClickHandler(archiveButtonClickHandler);
+    this._taskComponent.setFavoriteButtonClickHandler(favoriteButtonClickHandler);
 
-    render(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+    if (oldTaskComponent && oldTaskEditComponent) {
+      replace(this._taskComponent, oldTaskComponent);
+      replace(this._taskEditComponent, oldTaskEditComponent);
+    } else {
+      render(this._container, this._taskComponent, RenderPosition.BEFOREEND);
+    }
   }
 
   _replaceTaskWithEdit() {
