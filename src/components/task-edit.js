9f5eeb1526b7b9aaf7bf1271c1b349a19125d808
pepsiftd <pepsiftd@gmail.com>
2020-05-20 15:34:14 +0300

    Добавляет функцию reset в TaskEdit

--- src/components/task-edit.js
+++ src/components/task-edit.js
@@ -153,6 +153,16 @@ export default class TaskEdit extends AbstractSmartComponent {
     super.rerender();
   }
 
+  reset() {
+    const task = this._task;
+
+    this._isDateShowing = !!task.dueDate;
+    this._isRepeatingTask = Object.values(task.repeatingDays);
+    this._activeRepeatingDays = Object.assign({}, task.repeatingDays);
+
+    this.rerender();
+  }
+
   recoverListeners() {
     this.setSubmitHandler(this._submitHandler);
     this._subscribeOnEvents();
